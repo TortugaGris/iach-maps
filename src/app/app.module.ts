33b0c8b87268b23563d5722059f4df80a39517e7
Javier Zavala <javierzavalatorres@gmail.com>
2023-11-01 13:45:15 -0300

    feat: add church location map view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { ChurchLocationsMapComponent } from './components/church-locations-map/church-locations-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ChurchLocationsMapComponent
   ],
   imports: [
     BrowserModule,
