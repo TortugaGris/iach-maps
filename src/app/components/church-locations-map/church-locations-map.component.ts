a69780d2331bed8953f4b5adf94801507903c4a6
Javier Zavala <javierzavalatorres@gmail.com>
2023-11-01 14:29:42 -0300

    feat(church-locations-map): add map data

--- src/app/components/church-locations-map/church-locations-map.component.ts
+++ src/app/components/church-locations-map/church-locations-map.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { json } from 'd3';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
-  selector: 'app-church-locations-map',
-  templateUrl: './church-locations-map.component.html',
-  styleUrls: ['./church-locations-map.component.scss']
+    selector: 'app-church-locations-map',
+    templateUrl: './church-locations-map.component.html',
+    styleUrls: ['./church-locations-map.component.scss']
 })
 export class ChurchLocationsMapComponent {
+    @Input() width = 440;
+    @Input() height = 300;
 
+    constructor(private http: HttpClient) {
+
+    }
+
+    public data$ = this.getData();
+
+    private getData() {
+        const jsonUrl = 'https://raw.githubusercontent.com/TortugaGris/chile-geojson/master/regiones.json';
+        return this.http.get(jsonUrl);
+    }
 }
