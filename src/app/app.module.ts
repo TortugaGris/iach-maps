c81214f592adf57826ebf645d06eafa0e1af05d4
Javier Zavala <javierzavalatorres@gmail.com>
2023-11-01 14:28:34 -0300

    chore: add http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { ChurchLocationsMapComponent} from './components/church-locations-map/church-locations-map.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ChurchLocationsMapComponent} from './components/church-locations-map/ch
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
