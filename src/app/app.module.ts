50f18a1ba13d8c99f41edb9502cd3fd951394508
Javier Zavala <javierzavalatorres@gmail.com>
2023-11-01 13:50:26 -0300

    fix: show church location map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
-import { ChurchLocationsMapComponent } from './components/church-locations-map/church-locations-map.component';
+import { ChurchLocationsMapComponent} from './components/church-locations-map/church-locations-map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ChurchLocationsMapComponent
+    ChurchLocationsMapComponent,
   ],
   imports: [
     BrowserModule,
